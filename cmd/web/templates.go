137f8a3c3a599464016c0c621685e4d67296a688
Bas van der Hoeven <mail@basvdhoeven.nl>
2024-08-17 15:34:21 +0200

    embed html pages

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"html/template" // New import
+	"io/fs"
 	"path/filepath" // New import
+
+	"github.com/basvdhoeven/personal-website-go/ui"
 )
 
 type templateData struct {
@@ -13,7 +16,7 @@ type templateData struct {
 
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
-	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	pages, err := fs.Glob(ui.Files, "html/pages/*.tmpl")
 	if err != nil {
 		return nil, err
 	}
@@ -21,20 +24,17 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		files := []string{
-			"./ui/html/base.tmpl",
-			"./ui/html/partials/nav.tmpl",
+		patterns := []string{
+			"html/base.tmpl",
+			"html/partials/*.tmpl",
 			page,
 		}
 
-		// Parse the files into a template set.
-		ts, err := template.ParseFiles(files...)
+		ts, err := template.New(name).ParseFS(ui.Files, patterns...)
 		if err != nil {
 			return nil, err
 		}
 
-		// Add the template set to the map, using the name of the page
-		// (like 'home.tmpl') as the key.
 		cache[name] = ts
 	}
 
